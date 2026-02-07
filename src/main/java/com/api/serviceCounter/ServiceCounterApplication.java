4a2498a557bee74da5044f502d9919da11d579a3
Danilo Bruno <danilobsilv@gmail.com>
2026-02-06 22:23:06 -0400

    feat(#1): spring boot initial setup

---
+++ src/main/java/com/api/serviceCounter/ServiceCounterApplication.java
@@ -0,0 +1,13 @@
+package com.api.serviceCounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCounterApplication.class, args);
+	}
+
+}
